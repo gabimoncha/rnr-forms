926a55fb5e247e4cb2ae237ca0dcbd6c2ecd1d16
gabimoncha <gabimoncha@gmail.com>
2025-08-06 01:30:10 +0300

    update page header

--- apps/docs/app/(home)/page.tsx
+++ apps/docs/app/(home)/page.tsx
@@ -9,24 +9,27 @@ export default function HomePage() {
       className="max-w-fd-container mx-auto flex w-full flex-col items-center gap-4">
       <div className="container flex flex-col items-center gap-2 py-8 text-center md:py-16 lg:py-20 xl:gap-4">
         <h1 className="text-primary/90 leading-tighter max-w-3xl text-balance text-4xl font-semibold tracking-tight lg:leading-[1.1] xl:max-w-7xl xl:text-5xl xl:font-medium xl:tracking-tighter">
-          Copy-paste Ready Universal Forms
+          Copy-paste ready Universal Forms
         </h1>
+        <h3 className="text-muted-foreground leading-tighter max-w-3xl text-balance text-2xl font-semibold tracking-tight lg:leading-1 xl:max-w-7xl xl:text-3xl xl:font-medium xl:tracking-tighter">
+          Easy to install, customize and maintain
+        </h3>
         <figure className="flex flex-col gap-4">
           <Image
             src="/images/banner-light.svg"
             width="600"
             height="300"
             alt="Registry"
-            className="mt-6 overflow-hidden rounded-lg border dark:hidden"
+            className="mt-6 dark:hidden"
           />
           <Image
             src="/images/banner-dark.svg"
             width="600"
             height="300"
             alt="Registry"
-            className="mt-6 hidden overflow-hidden rounded-lg border shadow-sm dark:block"
+            className="mt-6 hidden shadow-sm dark:block"
           />
-          <figcaption className="text-center text-sm text-gray-500">
+          <figcaption className="text-center text-gray-500">
             A forms library for React Native and Expo apps
           </figcaption>
         </figure>
